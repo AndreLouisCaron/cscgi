fe1691b6ff3633a0e60ff8dde8915fda29587534
Andre Caron <andre.l.caron@gmail.com>
2012-10-23 20:33:44 -0700

    Buffers HTTP headers in libevent demo.

--- demo/libevent/scgi-libevent.c
+++ demo/libevent/scgi-libevent.c
@@ -38,12 +38,17 @@ struct connection_t
     // libevent data stream (socket).
     struct bufferevent * stream;
 
-    // custom data...
+    // Request buffers.
+    struct evbuffer * head;
+    struct evbuffer * body;
+
+    // TODO: add custom data...
 };
 
 // SCGI callback functions.
 static void accept_field (struct scgi_parser * parser,
                           const char * data, size_t size);
+static void finish_field (struct scgi_parser * parser);
 static void accept_value (struct scgi_parser * parser,
                           const char * data, size_t size);
 static void finish_value (struct scgi_parser * parser);
@@ -56,19 +61,23 @@ struct connection_t * prepare_connection ()
     // Allocate memory for bookkeeping.
     struct connection_t * connection = malloc(sizeof(struct connection_t));
     if (connection == NULL) {
+        fprintf(stderr, "Failed to allocate connection object.\n");
         return (NULL);
     }
     memset(connection, 0, sizeof(struct connection_t));
 
+    fprintf(stderr, "Setting up SCGI parser.\n");
+
     // Prepare the SCGI connection parser.
     struct scgi_limits limits =	{
-        .max_head_size =  2*1024,
-        .max_body_size = 64*1024,
+        .max_head_size =  4*1024,
+        .max_body_size = 60*1024,
     };
     scgi_setup(&limits, &connection->parser);
 
     // Register SCGI parser callbacks.
     connection->parser.accept_field = accept_field;
+    connection->parser.finish_field = finish_field;
     connection->parser.accept_value = accept_value;
     connection->parser.finish_value = finish_value;
     connection->parser.finish_head = finish_head;
@@ -80,13 +89,31 @@ struct connection_t * prepare_connection ()
     // Will be initialized later.
     connection->stream = 0;
 
+    // Prepare buffer for HTTP headers.
+    connection->head = evbuffer_new();
+    if (connection->head == NULL) {
+        fprintf(stderr, "Failed to allocate memory for HTTP head\n.");
+    }
+    if (evbuffer_expand(connection->head, 4*1024) != 0) {
+        fprintf(stderr, "Failed to allocate memory for HTTP headers\n.");
+    }
+
+    // Prepare buffer for HTTP body.
+    connection->body = evbuffer_new();
+    if (connection->body == NULL) {
+        fprintf(stderr, "Failed to allocate memory for HTTP body.\n");
+    }
+
     // TODO: initialize extra data.
 
+    fprintf(stderr, "Connection object ready.\n.");
     return (connection);
 }
 
 void release_connection (struct connection_t * connection)
-{
+{ 
+    fprintf(stderr, "Dropping connection.\n.");
+
     // Close socket.
     bufferevent_free(connection->stream);
 
@@ -94,32 +121,77 @@ void release_connection (struct connection_t * connection)
     free(connection);
 }
 
+
+// Buffer header name inside connection object.
 static void accept_field (struct scgi_parser * parser,
                           const char * data, size_t size)
 {
     struct connection_t * connection = parser->object;
-
-    // TODO: buffer header name inside connection object.
+    evbuffer_add(connection->head, data, size);
 }
 
+// Null-terminate the HTTP header name.
+static void finish_field (struct scgi_parser * parser)
+{
+    struct connection_t * connection = parser->object;
+    evbuffer_add(connection->head, "\0", 1);
+}
+
+// Buffer header data inside connection object.
 static void accept_value (struct scgi_parser * parser,
                           const char * data, size_t size)
 {
     struct connection_t * connection = parser->object;
-
-    // TODO: buffer header data inside connection object.
+    evbuffer_add(connection->head, data, size);
 }
 
+// Null-terminate the HTTP header data.
 static void finish_value (struct scgi_parser * parser)
 {
     struct connection_t * connection = parser->object;
-
-    // TODO: process HTTP header buffered inside connection object.
+    evbuffer_add(connection->head, "\0", 1);
 }
 
+// Double-null terminate the HTTP headers, start responding.
 static void finish_head (struct scgi_parser * parser)
 {
+    struct evbuffer_ptr here;
+    struct evbuffer_ptr next;
+    struct evbuffer_iovec name;
+    struct evbuffer_iovec data;
     struct connection_t * connection = parser->object;
+
+    fprintf(stderr, "Headers (done).\n");
+
+    // Iterate over HTTP headers.
+    memset(&here, 0, sizeof(here));
+    evbuffer_ptr_set(connection->head, &here, 0, EVBUFFER_PTR_SET);
+    next = evbuffer_search(connection->head, "\0", 1, &here);
+    while (next.pos >= 0)
+    {
+        // Locate the header name.
+        evbuffer_peek(connection->head, next.pos-here.pos, &here, &name, 1);
+        name.iov_len = next.pos-here.pos; // truncate.
+
+        // Locate the header data.
+        evbuffer_ptr_set(connection->head, &next, 1, EVBUFFER_PTR_ADD);
+        here = next;
+        next = evbuffer_search(connection->head, "\0", 1, &next);
+        evbuffer_peek(connection->head, next.pos-here.pos, &here, &data, 1);
+        data.iov_len = next.pos-here.pos; // truncate.
+
+        // Print out the HTTP header.
+        fprintf(stderr, "'%*s': '%*s'.\n",
+                (int)name.iov_len, (const char*)name.iov_base,
+                (int)data.iov_len, (const char*)data.iov_base);
+
+        // Locate the next header.
+        evbuffer_ptr_set(connection->head, &next, 1, EVBUFFER_PTR_ADD);
+        here = next;
+        next = evbuffer_search(connection->head, "\0", 1, &next);
+    }
+
+    fprintf(stderr, "Starting response.\n");
 
     // Start responding.
     struct evbuffer * output = bufferevent_get_output(connection->stream);
@@ -150,10 +222,11 @@ static void read_cb (struct bufferevent * stream, void * context)
     struct evbuffer * input = bufferevent_get_input(stream);
     size_t size = evbuffer_get_length(input);
     char * data = malloc(size);
+    fprintf(stderr, "Reading request data (%d bytes).\n", (int)size);
     if (evbuffer_remove(input, data, size) == -1)
     {
         // Log the error.
-        fprintf(stderr, "could not read data from the buffer\n");
+        fprintf(stderr, "Could not read data from the buffer.\n");
 
         // Drop connection.
         release_connection(connection);
@@ -165,6 +238,9 @@ static void read_cb (struct bufferevent * stream, void * context)
         return;
     }
  
+    fprintf(stderr, "Feeding request data:\n%*s\n",
+            (int)size, (const char*)data);
+
     // Feed the input data to the SCGI request parser.
     //   All actual processing is done inside the SCGI callbacks
     //   registered by our application.  Callbacks are always
@@ -180,6 +256,8 @@ static void read_cb (struct bufferevent * stream, void * context)
         release_connection(connection);
     }
 
+    fprintf(stderr, "Dropping request data.\n");
+
     // Release our copy of the input data.
     free(data), data = 0;
 }
@@ -192,7 +270,8 @@ static void write_cb (struct bufferevent * stream, void * context)
 
     if (evbuffer_get_length(output) == 0 &&
         evbuffer_get_length(input) == 0) {
-        release_connection(connection);
+        //fprintf(stderr, "Done with connection...\n");
+        //release_connection(connection);
     }
 }
 
@@ -200,6 +279,8 @@ static void echo_event_cb (struct bufferevent * stream,
                            short events, void * context)
 {
     struct connection_t * connection = context;
+
+    fprintf(stderr, "Error on socket.\n");
 
     if (events & BEV_EVENT_ERROR) {
         perror("Error from bufferevent");
@@ -213,22 +294,28 @@ static void accept_conn_cb (struct evconnlistener * listener,
                             evutil_socket_t socket,
                             struct sockaddr * peer, int size, void * context)
 {
+    fprintf(stdout, "Accepting connection!\n");
+
     // Prepare an SCGI request parser for the new connection.
     struct connection_t * connection = prepare_connection();
     if (connection == NULL) {
-        // TOOD: handle memory allocation failure (close socket?).
+        fprintf(stderr, "Failed to allocate connection object.\n");
     }
+
+    fprintf(stderr, "Getting stream.\n");
 
     // Prepare to read and write over the connected socket.
     struct event_base * base = evconnlistener_get_base(listener);
-    struct bufferevent * stream = bufferevent_socket_new(base, socket,
-                                                         BEV_OPT_CLOSE_ON_FREE);
+    struct bufferevent * stream = bufferevent_socket_new
+        (base, socket, BEV_OPT_CLOSE_ON_FREE);
     if (stream == NULL) {
-        // TODO: handle initialization failure.
+        fprintf(stderr, "Failed to allocate stream.\n");
     }
+    fprintf(stderr, "Configuring stream.\n");
     connection->stream = stream;
     bufferevent_setcb(stream, read_cb, write_cb, echo_event_cb, connection);
     bufferevent_enable(stream, EV_READ|EV_WRITE);
+    fprintf(stderr, "Stream ready.\n");
 }
 
 static void accept_error_cb (struct evconnlistener * listener, void * context)
